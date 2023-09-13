a98c4dc7f2eabb52ff7e16a953f5e871fed1f176
youredith <110220308+jieuncodes@users.noreply.github.com>
2023-09-13 20:25:48 +0900

    refactor: use react-query's useQuery

--- client/src/components/Market.tsx
+++ client/src/components/Market.tsx
@@ -1,34 +1,27 @@
 import { useEffect, useState } from "react";
 import { getMarketData } from "../services/binanceApi";
+import { useQuery } from "react-query";
+import { BinanceSymbol } from "../types/binance";
 
 const Market = () => {
-  const [marketData, setMarketData] = useState<any[]>([]);
-  const [loading, setLoading] = useState(true);
+  const {
+    data: marketData,
+    error,
+    isLoading,
+  } = useQuery<BinanceSymbol[]>("marketData", getMarketData);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getMarketData();
-        setMarketData(data);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (loading) {
+  if (isLoading) {
     return <div>Loading...</div>;
   }
-
+  if (error) {
+    return <div>Something went wrong</div>;
+  }
+  //TODO: show 10 and add pagination
   return (
     <div>
       <h1>Spot Trading Markets</h1>
       <ul>
-        {marketData.map((market, index) => (
+        {marketData?.slice(0, 10).map((market, index) => (
           <li key={index}>{market.symbol}</li>
         ))}
       </ul>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const rootElement = document.getElementById("root");
+const queryClient = new QueryClient();
 
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </React.StrictMode>
   );
 } else {
